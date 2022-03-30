28316a8c67b91d6102aeb85a6ebb959e71fe56f8
Jovansonlee Cesar <ivanceras@gmail.com>
2022-03-30 20:32:18 +0800

    more test, make the number more readable

--- common/src/sortable.rs
+++ common/src/sortable.rs
@@ -8,15 +8,31 @@ fn sort(array: &[U256]) -> Vec<U256> {
 
 #[test]
 fn already_sorted() {
-    let numbers = vec![U256::from(1i128), U256::from(2i128), U256::from(3i128)];
+    let numbers = vec![U256::from(1_i128), U256::from(2_i128), U256::from(3_i128)];
     let result = sort(&numbers);
     assert_eq!(result, numbers);
 }
 
 #[test]
 fn unsorted() {
-    let numbers = vec![U256::from(2i128), U256::from(1i128), U256::from(3i128)];
+    let numbers = vec![U256::from(2_i128), U256::from(1_i128), U256::from(3_i128)];
     let result = sort(&numbers);
-    let expected = vec![U256::from(1i128), U256::from(2i128), U256::from(3i128)];
+    let expected = vec![U256::from(1_i128), U256::from(2i128), U256::from(3_i128)];
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn large_numbers() {
+    let numbers = vec![
+        U256::from(212837128371931812_i128),
+        U256::from(81723812738912378812_i128),
+        U256::from(51623219381273_i128),
+    ];
+    let result = sort(&numbers);
+    let expected = vec![
+        U256::from(51623219381273_i128),
+        U256::from(212837128371931812_i128),
+        U256::from(81723812738912378812_i128),
+    ];
     assert_eq!(result, expected);
 }
